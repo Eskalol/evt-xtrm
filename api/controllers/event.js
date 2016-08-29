d0643608b8f08bb1a16b51f9bbd3fcb789187b88
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-08-29 08:14:32 +0200

    participant api controller created

--- api/controllers/event.js
+++ api/controllers/event.js
@@ -11,6 +11,13 @@ module.exports = {
 	updateEventById: updateEventById
 };
 
+/**
+ * lits all events no permission required visible for everyone
+ * 
+ * @param  {request}
+ * @param  {response}
+ * @return {response}
+ */
 function listEvents(req, res) {
 	Event.find({},
 		'_id title description limit date',
@@ -21,6 +28,15 @@ function listEvents(req, res) {
 	);
 }
 
+/**
+ * creates an event
+ *
+ * TODO: Only admins should be able to create an event
+ * 
+ * @param  {request}
+ * @param  {response}
+ * @return {response}
+ */
 function createEvent(req, res) {
 	Event.create(req.body, function(err, event) {
 		if (err || !event) { return res.send(400, { message: 'failed to add event' }); }
@@ -36,6 +52,13 @@ function createEvent(req, res) {
 	});
 }
 
+/**
+ * Gets an event by id no permission required visible for everyone
+ * 
+ * @param  {request}
+ * @param  {response}
+ * @return {response}
+ */
 function getEventById(req, res) {
 	console.log(req.swagger.params.id.value);
 	Event.findOne({ _id: req.swagger.params.id.value },
@@ -47,6 +70,15 @@ function getEventById(req, res) {
 	);
 }
 
+/**
+ * updates an event
+ *
+ * TODO: only admin should be able to update an event
+ * 
+ * @param  {request}
+ * @param  {response}
+ * @return {response}
+ */
 function updateEventById(req, res) {
 	Event.findOne({ _id: req.swagger.params.id.value }, function(err, event) {
 		if (err || !event) { return res.send(404, { message: "Not found" }); }
@@ -63,12 +95,21 @@ function updateEventById(req, res) {
 	});
 }
 
+/**
+ * deletes an event by id
+ *
+ * TODO: only admins should be able to delete an event
+ * 
+ * @param  {request}
+ * @param  {response}
+ * @return {response}
+ */
 function deleteEventById(req, res) {
-	Event.findOne({ _id: req.swagger.params.id.value }, function (err, event) {
-		if(err || !event) { return res.send(404, { message: 'Event not found' }); }
-	}).remove(function (err, event) {
-		console.log('hihi')
-		if (err) { return res.send(400, { message: 'could not delete event' }); }
-		else { return res.send(200, { message: 'removed event: ' + req.swagger.params.id.value }); }
-	})
+	Event.remove({
+		_id: req.swagger.params.id.value
+	}, function (err, status) {
+		if (status.result.n == 0 ) { return res.send(404, { message: 'Event not found' }); }
+		else if (err) { return res.send(400, { message: 'could not delete event' }); }
+		else { return res.send(200, { message: 'removed event' }); }
+	});
 }
