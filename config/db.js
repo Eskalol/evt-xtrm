30cee877f577c55bd83c6de7ead0fde3fcdfbad4
Eskil Opdahl Nordland <eskilon@student.matnat.uio.no>
2016-08-29 16:20:50 +0200

    lol

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
 
 // TODO: fix connection url from environment
-// //'mongodb://root:root@ds015636.mlab.com:15636/heroku_5pvfhc6r'
 module.exports = mongoose.createConnection(process.env.MONGO_URL || 'mongodb://test:test@localhost/evt-xtrm-db');
